5e11d1676a567ebf279b3905a820854548bdbae9
Avner Jose Guimarães Ribeiro <avnerjose@pop-os.localdomain>
2021-07-17 22:50:19 -0300

    Finishing boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,23 @@
-export function Main() {
-  return <h1>React Avançado</h1>;
+import { Container, Title, Description, Logo, Illustration } from "./styles";
+
+type MainProps = {
+  title?: string;
+  description?: string;
+};
+
+export function Main({
+  title = "React avançado",
+  description = "Typescript, ReactJs, NextJs e Styled-Components"
+}: MainProps) {
+  return (
+    <Container>
+      <Logo src="/img/logo.svg" alt="React Avançado" />
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela"
+      />
+    </Container>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
-import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { GlobalStyles } from "../styles/globals";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project started to work with typescriplt"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
